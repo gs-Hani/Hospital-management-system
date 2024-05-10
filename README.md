1d49c3cd8d6c09edf1301936155687f106ffb24c
gs-Hani <hani_alazawi@hotmail.com>
2024-05-10 08:39:10 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hospital managment system #
+
+##  Description ##
+
+## Table of Contents ##
+Too short of a README file to have one of those.
+
+## How to install/run ##
+
+## How to use ##
+
+## Credits ##
+n/a
+
+## License ##
+The GNU General Public License
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	int option,id;
+	char yn;
+	char c;
+	int flag=0;
+	system("clear");
+	printf("\n\n\n\n\t\t\t\t\t*******************************\t\tWELCOME TO WE-CARE\t*******************************\n\n");
+	return 0;
+}
