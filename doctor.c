d4912805c59a411559c0d2b250e1d8f2dc7969b7
gs-Hani <hani_alazawi@hotmail.com>
2024-07-25 07:37:14 +0400

    add a doctor function

--- doctor.c
+++ doctor.c
@@ -1,3 +1,50 @@
-int doctor () {
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
+#include "extern.h"
+#include "print.h"
+
+int doctor () {
+	char c,d_name[100];
+	while((c=getchar())!='\n' && c != EOF)
+		;
+again:	printf("Enter doctor's name: ");
+	fgets(d_name,100,stdin);
+	FILE* fptr=fopen("records.dat", "rb")
+		;
+	char dummy[100];
+	int flag=0
+		;
+	for(int i=0;i<strlen(d_name);i++) {
+		d_name[i]=tolower(d_name[i]);
+	}
+
+	FILE *doc = fopen("doctor.dat", "wb+");
+	struct patient p
+		;
+	while (fread(&p, sizeof(struct patient), 1, fptr)==1) {
+
+		for(int i=0; i<strlen(p.doc);i++) {
+			dummy[i]=tolower(p.doc[i]);
+		}
+	
+		if(strstr(dummy, d_name)) {
+			fwrite(&p, sizeof(struct patient), 1, doc);
+			flag=1;
+		}		
+	}
+
+	if (flag==0) {
+		printf("Doctor not found.\n");
+		fclose(doc);
+		fclose(fptr);
+		goto again;
+	} else {
+		fseek(doc, 0, SEEK_SET);
+		print(doc);
+		fclose(doc);
+		fclose(fptr);
+		return 0;
+	}
 }
